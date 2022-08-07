2c7f1520365a4e31ee41d1cef7b4d7ed77c1f7c1
Soptq <32592090+soptq@users.noreply.github.com>
2022-08-07 23:03:26 +0800

    feat: add an *extremely simple* frontend

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pickle
 import config
 from tqdm import tqdm
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from classifier import ManualGasPriceClassifier, BlocknumIntervalClassifier, SameSumTxnsClassifier
 
@@ -38,6 +39,46 @@ for contract_address, transactions in dataset.items():
             classifier.process(contract_address, transaction_hash, data["txn"], data["receipt"])
 
 app = FastAPI()
+
+
+@app.get("/")
+def index():
+    html_content = """
+        <html>
+            <head>
+                <title>Sacred DAR(Deposit Address Reuse) Analysis</title>
+            </head>
+            <body>
+                <h1>Sacred DAR(Deposit Address Reuse) Analysis</h1>
+                <p>This page will tell you addresses that is considered to be related with the address you input. The data source is Sacred Finance kovan testnet</p>
+                <p>It currently contains 4 basic rules, including 1. manually set gas price 2. small interval between deposit and withdraw 3. applies the same address for depositing and withdrawing and 4. the same number of deposits and withdraws (e.g. a user deposits 10 times using address A1 and then withdraws 10 times using address B1, then A1 and B1 might be linked).</p>
+                <strong>Input an address to query</strong><br/><br/>
+                <input type="text" id="address" placeholder="Enter an address">
+                <button onclick="get_result()">Submit</button><br/><br/><br/>
+                <strong>Related addresses</strong><br/><br/>
+                <div id="result"></div>
+                <script>
+                    function get_result() {
+                        var address = document.getElementById("address").value;
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("GET", "/classify/" + address, true);
+                        xhr.send();
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState == 4) {
+                                var response = JSON.parse(xhr.responseText);
+                                if (response["result"].length == 0) {
+                                    document.getElementById("result").innerHTML = "No related addresses found";
+                                } else {
+                                    document.getElementById("result").innerHTML = response["result"].join("<br/>");
+                                }
+                            }
+                        }
+                    }
+                </script>
+            </body>
+        </html>
+        """
+    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/classify/{address}")
